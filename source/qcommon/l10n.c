597ee1e9889e36e613d3df6d336b3e8d81e653e4
Warfork <51063252+Warfork@users.noreply.github.com>
2023-05-17 16:50:41 -0400

    move l10n to qcommon

--- source/qcommon/l10n.c
+++ source/qcommon/l10n.c
@@ -18,7 +18,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
 
-#include "client.h"
+#include "qcommon.h"
 #include "../qalgo/q_trie.h"
 #include "l10n.h"
 
--- source/qcommon/qcommon.h
+++ source/qcommon/qcommon.h
@@ -35,6 +35,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "cmodel.h"
 #include "version.h"
 #include "bsp.h"
+#include "l10n.h"
 
 //#define	PARANOID			// speed sapping error checking
 
