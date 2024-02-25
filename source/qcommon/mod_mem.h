619298040aa6bb9cf246624a82b95c3e49eb6694
Michael Pollind <mpollind@gmail.com>
2024-02-24 23:44:24 -0800

    fix module
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

--- source/qcommon/mod_mem.h
+++ source/qcommon/mod_mem.h
@@ -43,8 +43,8 @@ void *__Q_Malloc( size_t size, const char *sourceFilename, const char *functionN
 void *__Q_Realloc( void *ptr, size_t size, const char *sourceFilename, const char *functionName, int sourceLine ) { return mem_import.__Q_Realloc(ptr, size, sourceFilename, functionName, sourceLine); }
 void *__Q_MallocAligned( size_t alignment, size_t size, const char *sourceFilename, const char *functionName, int sourceLine ) { return mem_import.__Q_MallocAligned(alignment, size, sourceFilename, functionName, sourceLine); }
 void Q_Free( void *ptr ) { return mem_import.Q_Free(ptr); }
-mempool_t *Q_CreatePool( mempool_t *parent, const char *name ) { return mem_import.Q_CreatePool(parent ? mem_import.parent : parent, name);}
-void Q_LinkToPool( void *ptr, mempool_t *pool ) {return mem_import.Q_LinkToPool(ptr, parent ? mem_import.parent : parent);}
+mempool_t *Q_CreatePool( mempool_t *parent, const char *name ) { return mem_import.Q_CreatePool(parent ? parent : mem_import.parent , name);}
+void Q_LinkToPool( void *ptr, mempool_t *pool ) {return mem_import.Q_LinkToPool(ptr, parent ? parent : mem_import.parent);}
 void Q_FreePool( mempool_t *pool ) {mem_import.Q_FreePool(pool);}
 void Q_EmptyPool( mempool_t *pool ) {mem_import.Q_EmptyPool(pool);}
 #endif
