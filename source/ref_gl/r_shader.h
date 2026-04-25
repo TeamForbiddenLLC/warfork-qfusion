2b64bf7b059582d14e9e3ebf443467c5c8bba922
astroZEBUS <iamzebus@gmail.com>
2026-04-25 09:57:56 -0400

    Increased max animmap frames to 32, used for weapon textures.

--- source/ref_gl/r_shader.h
+++ source/ref_gl/r_shader.h
@@ -23,7 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define MAX_SHADERS					2048
 #define MAX_SHADER_PASSES			8
 #define MAX_SHADER_DEFORMVS			8
-#define MAX_SHADER_IMAGES			16
+#define MAX_SHADER_IMAGES			32
 #define MAX_SHADER_TCMODS			8
 
 // shader types (by superlightstyle)
