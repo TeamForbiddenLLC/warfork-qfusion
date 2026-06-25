a611927b4d306450f7ada8632743d0e3dddaeeca
Michael Pollind <mpollind@gmail.com>
2026-06-25 15:51:41 -0700

    feat: fix fullscreen for gl renderer (#568)
    upstream changes from qfusion:
    Qfusion/qfusion@16f0ccec1a9ad3231623f376da5e5b13038e28c8
       SDL_GL_DeleteContext in GLimp_Shutdown + local-context/fail-path cleanup in GLimp_InitGL (sdl_glw.c); the vid_xpos/vid_ypos ≥ 8 title-bar clamp (cl_vid.c)
    Qfusion/qfusion@54df98cc8a09273f377af8460ef55e949fc940d5
       The GLimp_SetFullscreenMode → GLimp_SetFullscreen(fullscreen, xpos,ypos) rename + SDL_SetWindowPosition after the fullscreen toggle (sdl_glw.c, r_glimp.h, r_frontend.c, win_glw.c, unix_glw.c). The win32 WM_GETMINMAXINFO half was not ported
    
    Signed-off-by: Michael Pollind <mpollind@gmail.com>

--- source/ref_gl/r_glimp.h
+++ source/ref_gl/r_glimp.h
@@ -244,7 +244,7 @@ int		GLimp_Init( const char *applicationName, void *hinstance, void *wndproc, vo
 void	GLimp_Shutdown( void );
 rserr_t	GLimp_SetMode( int x, int y, int width, int height, int displayFrequency, bool fullscreen, bool stereo );
 rserr_t	GLimp_SetWindow( void *hinstance, void *wndproc, void *parenthWnd, bool *surfaceChangePending );
-rserr_t GLimp_SetFullscreenMode( int displayFrequency, bool fullscreen );
+rserr_t GLimp_SetFullscreen( bool fullscreen, int xpos, int ypos );
 void	GLimp_AppActivate( bool active, bool destroy );
 bool	GLimp_GetGammaRamp( size_t stride, unsigned short *psize, unsigned short *ramp );
 void	GLimp_SetGammaRamp( size_t stride, unsigned short   size, unsigned short *ramp );
